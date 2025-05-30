c7f1bb30063b87364d05121170199426784c6134
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-05-30 23:13:30 +0200

    feat(progressbar): add visual progress bar to show user completion status

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes";
 import { GameProgressProvider } from "./context/GameprogressProvider";
+import Progressbar from "./assets/Progressbar/Progressbar";
+
 function App() {
     return (
         <GameProgressProvider>
             <BrowserRouter>
                 <AppRoutes />
             </BrowserRouter>
+            <Progressbar />
         </GameProgressProvider>
     );
 }
