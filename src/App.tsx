d957214f0dfe87a21ec69d5cc2d70012acc6937c
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-01 16:05:06 +0200

    refactor(structure): move games and project components into a components folder for better clarity and structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes";
 import { GameProgressProvider } from "./context/GameprogressProvider";
-import Progressbar from "./assets/Progressbar/Progressbar";
-import Header from "./assets/Header/Header";
+import Progressbar from "./components/Progressbar/Progressbar";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
